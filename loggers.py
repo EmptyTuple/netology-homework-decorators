83ac0d4c7673b3d2304edf0bd6815d237b25f810
EmptyTuple <kotorov@outlook.com>
2022-06-19 22:21:53 +0700

    first commit

---
+++ loggers.py
@@ -0,0 +1,39 @@
+from functools import wraps
+from datetime import datetime
+import os
+
+def just_logger(func):
+    
+    @wraps(func)
+    def log_it(*args, **kwargs):
+        with open('logfile.log', 'a') as f:
+            f.write(datetime.now().strftime('%Y-%m-%d %H:%S:%f') + '\n')
+            f.write(f'The function "{func.__name__}" was called,\n')
+            f.write(f'with arguments: {args}{kwargs},\n')
+            res = func(*args, **kwargs)
+            f.write(f'{res} was returned.\n---------\n')
+            return res
+        
+    return log_it
+
+def logger_with_path(path_to_log):
+    def _logger_with_path(func):
+        if not os.path.isdir(path_to_log):
+            os.makedirs(path_to_log)
+        
+        @wraps(func)
+        def log_it(*args, **kwargs):
+            with open(f'{path_to_log}/logfile.log', 'a') as f:
+                f.write(datetime.now().strftime('%Y-%m-%d %H:%S:%f') + '\n')
+                f.write(f'The function "{func.__name__}" was called,\n')
+                f.write(f'with arguments: {args}{kwargs},\n')
+                res = func(*args, **kwargs)
+                f.write(f'{res} was returned.\n---------\n')
+                return res
+            
+        return log_it
+    return _logger_with_path
+
+print(os.getcwd())
+
+# os.makedirs('nested/nested')
---
+++ main.py
@@ -0,0 +1,42 @@
+import itertools
+from loggers import just_logger, logger_with_path
+
+# Use the previous homework functions
+
+@just_logger
+def flatten_single_nested(iterable: list | tuple)-> tuple:
+    
+    try:
+        flat_thing = tuple(itertools.chain.from_iterable(iterable))
+        return flat_thing
+    except Exception as ex:
+        print('There is a non-symmetric nesting in the iterable!')
+        
+@logger_with_path(path_to_log='tmp/logs')
+def flatten_many_nested(iterable: list | tuple)-> tuple:
+    
+    try:
+        flat_thing = list(itertools.chain.from_iterable(iterable))
+        if any(map(lambda x: type(x) == list, flat_thing)) is True:
+            return flatten_many_nested(flat_thing)
+        return tuple(flat_thing)
+    except Exception as ex:
+        print('There is a non-symmetric nesting in the iterable!')
+
+def main():
+    single_nested = [
+	    ['a', 'b', 'c'],
+	    ['d', 'e', 'f', 'h', False],
+	    [1, 2, None]
+    ]
+
+    triple_nested = [
+        [[['we', 'all'], ['live', 'in']], [['the', 'yellow'], ['submarine', 'yellow', 'submarine']]],
+        [[['we', 'all'], ['live', 'in']], [['the', 'yellow'], ['submarine', 'yellow', 'submarine']]]
+    ]
+
+    flatten_single_nested(single_nested)
+    flatten_many_nested(triple_nested)
+
+if __name__ == '__main__':
+    main()
